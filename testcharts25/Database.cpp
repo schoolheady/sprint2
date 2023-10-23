30377ec0c005572228f77a134bae0c15e4c48860
AhmadS0099 <146102190+AhmadS0099@users.noreply.github.com>
2023-10-23 15:07:31 +0200

    v5
    Full code with Drivers

---
+++ testcharts25/Database.cpp
@@ -0,0 +1,47 @@
+#include "Database.h" // Include custom Database header file
+
+// Function to connect to the database
+void Database::connect(const std::string& dbName)
+{
+    db = QSqlDatabase::addDatabase("QMYSQL"); // Add a MySQL database
+    db.setHostName("127.0.0.1"); // Set the host name to localhost
+    db.setDatabaseName(QString::fromStdString(dbName)); // Set the database name
+    db.setUserName("root"); // Set the username
+    db.setPassword(""); // Set the password
+    db.open(); // Open the database connection
+}
+
+// Function to check if the database is open
+bool Database::isOpen()
+{
+    return db.isOpen(); // Return true if the database is open, false otherwise
+}
+
+// Function to fetch data from the database
+void Database::fetchData(std::vector<int>& ArrayTemp, std::vector<float>& array2, std::vector<float>& array3, QVector<QTime>& timeArray)
+{
+    QSqlQuery query; // Create a QSqlQuery object
+    QString queryString = "SELECT Tijd2, SensorTemp, SensorDruk, SensorVocht FROM testqt "
+                          "WHERE Tijd2 >= NOW() - INTERVAL 24 HOUR "
+                          "ORDER BY Tijd2 ASC"; // Define the SQL query string
+
+    if (query.exec(queryString)) { // Execute the SQL query
+        while (query.next()) { // Loop through the query results
+            QTime timeValue = QTime::fromString(query.value(0).toString(), "hh:mm:ss"); // Get the time value from the query result
+            timeArray.push_back(timeValue); // Add the time value to the timeArray vector
+            ArrayTemp.push_back(query.value(1).toInt()); // Add the temperature value to the ArrayTemp vector
+            array2.push_back(query.value(2).toFloat()); // Add the pressure value to the array2 vector
+            array3.push_back(query.value(3).toFloat()); // Add the humidity value to the array3 vector
+        }
+    } else {
+        qDebug() << "Error:" << query.lastError().text(); // Print error message if unable to execute the SQL query
+    }
+
+    qDebug() << "de eerste array " << timeArray[0]; // Print the first element of timeArray vector
+}
+
+// Function to close the database connection
+void Database::close()
+{
+    db.close(); // Close the database connection
+}
---
+++ testcharts25/chartfunctions.h
@@ -0,0 +1,32 @@
+#ifndef CHARTFUNCTIONS_H
+#define CHARTFUNCTIONS_H
+
+#include <QtCharts>
+#include <QTime>
+#include <QVector>
+#include <QValueAxis>
+#include <QDebug>
+#include "Database.h"
+
+// Global variables for storing fetched data and database object
+extern std::vector<int> Array;
+extern std::vector<float> array2;
+extern std::vector<float> array3;
+extern QVector<QTime> timeArray;
+extern Database db;
+
+// Unique pointers to QLineSeries objects for storing series data for the chart
+extern std::unique_ptr<QLineSeries> series1;
+extern std::unique_ptr<QLineSeries> series2;
+extern std::unique_ptr<QLineSeries> series3;
+
+// Connects to the database and fetches data
+void databaseConnection();
+
+// Adds fetched data to the series
+void addDataToSeries();
+
+// Creates and sets axes for the chart
+void createAndSetAxes(QChart* chart);
+
+#endif // CHARTFUNCTIONS_H
